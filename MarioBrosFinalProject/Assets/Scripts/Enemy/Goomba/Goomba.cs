16cae40aaec5c95997a84bf67b22aacde988aacf
santurti <116548628+santurti@users.noreply.github.com>
2025-01-08 22:32:43 +0100

    re organizing enemies

--- MarioBrosFinalProject/Assets/Scripts/Enemy/Goomba/Goomba.cs
+++ MarioBrosFinalProject/Assets/Scripts/Enemy/Goomba/Goomba.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoombaMovement : MonoBehaviour
+public class Goomba : MonoBehaviour
 {
     private Rigidbody2D rb;
     public PlayerController Mario;
