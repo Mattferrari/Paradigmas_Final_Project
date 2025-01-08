bf947b5ac66d17f39d4c06efb9b50da936c09068
santurti <116548628+santurti@users.noreply.github.com>
2025-01-08 17:22:35 +0100

    koopa Patroopa movement
    + vertical movement of patroopa

--- MarioBrosFinalProject/Assets/Scripts/Enemy/Koopa/KoopaPatroopa.cs
+++ MarioBrosFinalProject/Assets/Scripts/Enemy/Koopa/KoopaPatroopa.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class KoopaPatroopa : MonoBehaviour, IEnemy
 {
-    public PlayerController Mario;
-    public Rigidbody2D rb;
+    private PlayerController Mario;
+    private Rigidbody2D rb;
 
     //going up(1) or down(-1)
     private int movedir = 1;
@@ -15,9 +15,6 @@ public class KoopaPatroopa : MonoBehaviour, IEnemy
     [SerializeField] private int miny;
 
     private float speed = 2f;
-    // When koopaPatroopa reaches the extremes of the movement range will stop for 250 ms before changing dir
-    private float stopTime = 0.25f;
-    private bool isStopped = false;
 
     public Collider2D superiorCollider;
     public Collider2D lowerCollider;
@@ -35,35 +32,27 @@ public class KoopaPatroopa : MonoBehaviour, IEnemy
     public void Move()
     {
         Vector2 PatroopaPosition = transform.position;
-        bool onEdge = PatroopaPosition.y >= maxy || PatroopaPosition.y <= miny;
-        if (onEdge && !isStopped)
+        if (PatroopaPosition.y >= maxy)
         {
-            StartCoroutine(Stop());
+            movedir = -1;
         }
-        else if (!isStopped)
+        else if (PatroopaPosition.y <= miny)
         {
-            rb.velocity = new Vector2(rb.velocity.x, movedir * speed);
+            movedir = 1;
         }
+        rb.velocity = new Vector2(rb.velocity.x, movedir * speed);
     }
 
-    // function to stop the koopa 
-    private IEnumerator Stop()
-    {
-        isStopped = true;
-        yield return new WaitForSeconds(stopTime);
-        isStopped = false;
-        movedir *= -1;
-    }
     // Start is called before the first frame update
     void Start()
     {
         Mario = GetComponent<PlayerController>();
-        GetComponent<Rigidbody>().useGravity = false;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
 }
