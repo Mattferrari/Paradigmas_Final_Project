209a5cfef7c92c75d421933bbc9590a3be84c837
santurti <116548628+santurti@users.noreply.github.com>
2025-01-08 17:51:26 +0100

    patroopa colisios
    + added colisions logic to patroopa

--- MarioBrosFinalProject/Assets/Scripts/Enemy/Koopa/KoopaPatroopa.cs
+++ MarioBrosFinalProject/Assets/Scripts/Enemy/Koopa/KoopaPatroopa.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class KoopaPatroopa : MonoBehaviour, IEnemy
 {
     private PlayerController Mario;
     private Rigidbody2D rb;
-
+    private GameObject targetPrefab;
     //going up(1) or down(-1)
     private int movedir = 1;
     
@@ -19,14 +20,15 @@ public class KoopaPatroopa : MonoBehaviour, IEnemy
     public Collider2D superiorCollider;
     public Collider2D lowerCollider;
 
+
     public void Atack()
     {
-
+        Mario.GetHit();
     }
     
     public void GetKilled()
     {
-
+        SwitchPrefab();
     }
     
     public void Move()
@@ -43,16 +45,38 @@ public class KoopaPatroopa : MonoBehaviour, IEnemy
         rb.velocity = new Vector2(rb.velocity.x, movedir * speed);
     }
 
+
+    public void SwitchPrefab()
+    {
+        Vector3 position = transform.position;
+        Quaternion rotation = transform.rotation;
+        Destroy(gameObject); 
+        GameObject newPrefab = Instantiate(targetPrefab, position, rotation); 
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         Mario = GetComponent<PlayerController>();
         rb = GetComponent<Rigidbody2D>();
+        targetPrefab = Resources.Load<GameObject>("Prefabs/Enemies/Koopa/Koopa");
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("player"))
+        {
+            if (collision.otherCollider == lowerCollider) { Atack(); }
+            else { GetKilled(); }
+        }
+        else { movedir *= -1; }
+        
     }
 }
